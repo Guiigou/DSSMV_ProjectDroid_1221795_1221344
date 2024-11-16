1ddc65dbdf6834dd998ec3215b0126cecb1652df
Guilherme Gouveia <guigoumain@gmail.com>
2024-11-16 00:22:23 +0000

    Implmentação de Reconhecimento por voz do livro

--- app/src/main/java/com/example/libraryapplication/activities/BooksActivity.java
+++ app/src/main/java/com/example/libraryapplication/activities/BooksActivity.java
@@ -8,7 +8,6 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
